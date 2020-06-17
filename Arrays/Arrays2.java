19a9f9c116ce5eaa536cff8272ed0abba1f7ea5d
vigashini <66862429+vigashini@users.noreply.github.com>
2020-06-17 14:18:25 +0530

    Add files via upload

---
+++ Arrays/Arrays2.java
@@ -0,0 +1,10 @@
+public class Arrays2
+{
+public static void main(String args[])
+{
+int arr[]={3,7,4,8};
+Arrays.sort(arr);
+System.out.println("Minimum value:"+arr[0]+" Maximum value:"+arr[arr.length-1]);
+}
+}
+
---
+++ Arrays/Arrays4.java
@@ -0,0 +1,16 @@
+public class Array4
+{
+public static void main(String args[])
+{
+int arr[]={65,97,66,100};
+char ch[]=new char[arr.length];
+for(int i=0;i<arr.length;i++)
+{
+ch[i]=(char)arr[i];
+}
+for(int i=0;i<arr.length;i++)
+{
+System.out.println(ch[i]+" ");
+}
+}
+}
